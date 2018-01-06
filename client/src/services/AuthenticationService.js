55993553a1479ca3fce336c8d2535337806eac72
Daniel Lindskoge <daniel@waymoot.se>
2018-01-06 15:08:22 +0100

    Setting up the registration page, end of part 1

--- client/src/services/AuthenticationService.js
+++ client/src/services/AuthenticationService.js
@@ -2,7 +2,7 @@ import Api from '@/services/Api'
 
 export default {
     register (credentials) {
-        return Api().post('register',credentials)
+        return Api().post('register', credentials)
     }
 }
 
--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,13 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combined'))
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.post('/register', (req, res) => {
-    console.log("req ="+ req.body.email)
     res.send({
-        message: `Hello ${req.body.email}! Your user was registered, have fun!`
+        message: `Hello ${req.body.email}! Your user was registered! Have fun!`
     })
 })
 
